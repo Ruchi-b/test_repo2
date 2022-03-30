6521eab95a24a95c4b0d464b652c525241d8afa4
RB66165 <RB66165@RMGW63ZLPC0427.ind.zensar.com>
2022-03-30 16:10:24 +0530

    This is Git repo

---
+++ eurekaserver1/src/main/java/com/zensar/eurekaserver/Eurekaserver1Application.java
@@ -0,0 +1,18 @@
+package com.zensar.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class Eurekaserver1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eurekaserver1Application.class, args);
+	}
+
+}
